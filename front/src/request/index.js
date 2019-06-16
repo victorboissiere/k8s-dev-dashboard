1f1adc4ae66d0a45c5a79c8b5d4a12a2bc392c6a
Victor Boissiere <victor.boissiere@neo9.fr>
2019-06-16 15:11:33 +0200

    k8s: Add deployments specs

--- front/src/request/index.js
+++ front/src/request/index.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default {
   make: (method, path) => axios({
     method,
-    url: `${process.env.API_BASE_URL || 'http://localhost:3000'}/v1/${path}`,
+    url: `${process.env.VUE_APP_API_BASE_URL || 'http://localhost:3000'}/v1/${path}`,
   }),
 };
